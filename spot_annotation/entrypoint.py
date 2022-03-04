c1121a3235697b1d5bfe93fc573629d9ab5c9684
Adrien Boucaud <adrien.bk@free.fr>
2022-03-04 14:59:42 +0100

    Support generating preview and interface data

--- spot_annotation/entrypoint.py
+++ spot_annotation/entrypoint.py
@@ -1,18 +1,80 @@
+import base64
 import argparse
 import json
 import sys
 
 from operator import itemgetter
 
+import numpy as np # library for array manipulation
+
 import annotation_client.annotations as annotations
 import annotation_client.tiles as tiles
+import annotation_client.workers as workers
 
 import imageio
 
-import numpy as np # library for array manipulation
 from skimage import filters
 from skimage.feature import peak_local_max
 
+
+def preview(datasetId, apiUrl, token, params, image):
+    # Setup helper classes with url and credentials
+    client = workers.UPennContrastWorkersClient(apiUrl=apiUrl, token=token)
+    datasetClient = tiles.UPennContrastDataset(
+        apiUrl=apiUrl, token=token, datasetId=datasetId)
+    
+    keys = ["assignment", "channel", "connectTo", "tags", "tile", "workerInterface"]
+    assignment, channel, connectTo, tags, tile, workerInterface = itemgetter(*keys)(params)
+    thresholdValue = workerInterface['threshold']['value']
+
+    # Get the tile
+    pngBuffer = datasetClient.getRawImage(tile['XY'], tile['Z'], tile['Time'], channel)
+    pngImage = imageio.imread(pngBuffer)
+
+    # Compute the threshold indexes
+    index = pngImage > thresholdValue
+
+    # Convert image to RGB
+    rgb = np.stack((pngImage,) * 3, axis=-1)
+
+    # Paint threshold areas red
+    rgb[index] = [255, 0, 0]
+
+    # Generate an output data-uri from the threshold image
+    outputPng = imageio.imwrite('<bytes>', rgb, format='png')
+    data64 = base64.b64encode(outputPng)
+    dataUri = 'data:image/png;base64,' + data64.decode('ascii')
+
+    # Send the preview object to the server
+    preview = {
+        'text': workerInterface['someText']['value'],
+        'image': dataUri
+    }
+    client.setWorkerImagePreview(image, preview)
+
+def interface(image, apiUrl, token):
+    client = workers.UPennContrastWorkersClient(apiUrl=apiUrl, token=token)
+
+    # Available types: number, text, tags, layer
+    interface = {
+        'threshold': {
+            'type': 'number',
+            'min': 0,
+            'max': 255,
+            'default': 50
+        },
+        'someTags': {
+            'type': 'tags'
+        },
+        'someLayer': {
+            'type': 'layer'
+        },
+        'someText': {
+            'type': 'text'
+        }
+    }
+    # Send the interface object to the server
+    client.setWorkerImageInterface(image, interface)
 
 def main(datasetId, apiUrl, token, params):
     """
@@ -29,16 +91,26 @@ def main(datasetId, apiUrl, token, params):
         tags: annotation tags (list of strings),
         tile: tile position (TODO: roi) ({XY, Z, Time}),
         connectTo: how new annotations should be connected
+        request: 'compute', 'preview', 'interface'
+        interface: dictionary containing parameters associated to their ids
     """
+
+    # Check whether we need to preview, send the interface, or compute
+    request = params.get('request', 'compute')
+    if request == 'preview':
+        return preview(datasetId, apiUrl, token, params, params['image'])
+    if request == 'interface':
+        return interface(params['image'], apiUrl, token)
+
     # roughly validate params
-    keys = ["assignment", "channel", "connectTo", "tags", "tile"]
+    keys = ["assignment", "channel", "connectTo", "tags", "tile", "workerInterface"]
     if not all (key in params for key in keys):
         print ("Invalid worker parameters", params)
         return
-    assignment, channel, connectTo, tags, tile = itemgetter(*keys)(params)
+    assignment, channel, connectTo, tags, tile, workerInterface = itemgetter(*keys)(params)
 
-    # TODO: completely arbitrary
-    threshold=0.0001
+    # Get the threshold from interface values
+    threshold=workerInterface['threshold']['value']
 
     # Setup helper classes with url and credentials
     annotationClient = annotations.UPennContrastAnnotationClient(
@@ -91,7 +163,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Compute average intensity values in a circle around point annotations')
 
-    parser.add_argument('--datasetId', type=str, required=True, action='store')
+    parser.add_argument('--datasetId', type=str, required=False, action='store')
     parser.add_argument('--apiUrl', type=str, required=True, action='store')
     parser.add_argument('--token', type=str, required=True, action='store')
     parser.add_argument('--parameters', type=str,
